9177d31ed5494e9f470c92d32501cb1c3ae063cf
AManOfFortune <101577584+AManOfFortune@users.noreply.github.com>
2022-12-03 03:04:05 +0100

    Fixed compile error of last merge

--- Assets/Scripts/Actionfields/ExtraMoveField.cs
+++ Assets/Scripts/Actionfields/ExtraMoveField.cs
@@ -6,7 +6,8 @@ public class ExtraMoveField : Actionfield
 {
     public override void performAction(Stone Piece)
     {
-        Piece.MovePiece(-1);
+        //Piece.MovePiece(-1);
+        Debug.Log("Yeah I don't work (anymore) but its fine, once Raoul goes crazy this would've needed to be changed anyway");
     }
 
     public override void changeVisuals(GameObject Node)
