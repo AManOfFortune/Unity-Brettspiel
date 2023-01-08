ddc3a94df72eb2e41517b548751b4ebea12599b5
Felix <51886988+Felix-beep@users.noreply.github.com>
2023-01-08 18:39:04 +0100

    Improved Gizmo Drawing on Vortex
    Each Vortex now draws its own Arrows, instead of drawing them recursively

--- Assets/Scripts/Graph/Vertex.cs
+++ Assets/Scripts/Graph/Vertex.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class Vertex : Node
 {
-    public bool AddedToList = false;
     public GameObject[] adjacentVertices;
+
+    private void OnDrawGizmos()
+    {
+        // Store the position of the current vertex
+        var currentPos = this.transform.position;
+             
+        foreach (var adjacentVertex in adjacentVertices)
+        {
+            // Store the direction of the adjacent vertex
+            var adjacentVertexDir = adjacentVertex.transform.position;
+            // Draw an arrow from the current vertex in the direction of the adjacent vertex
+            DrawArrow.ForGizmo(currentPos, adjacentVertexDir - currentPos, Color.green);
+            
+        }
+    }
 }
